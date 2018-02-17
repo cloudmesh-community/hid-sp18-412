dcbef1fab934806d2b64693a2aca7b73e8d6b683
karankotz <karankotz@gmail.com>
2018-02-17 14:44:11 -0500

    Deleted old file

--- swagger/Cloudmesh/keyvaluestore/server/cpu/flaskConnexion/swagger_server/controllers/keyval_stub.py
+++
@@ -1,8 +0,0 @@
-from firebase import firebase 
-
-firebase = firebase.FirebaseApplication('https://swagger-97c12.firebaseio.com/')
-
-def get_keyvalstore():
-	#firebase = firebase.FirebaseApplication('https://swagger-97c12.firebaseio.com/')
-	result =firebase.get('/User',None)
-	return result
