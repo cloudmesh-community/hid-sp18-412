8add0fc98610ada74be42939eaaaaaf7ccaad814
karankotz <karankotz@gmail.com>
2018-04-21 00:26:32 -0400

    Updated keygen script for master

---
+++ project-code/scripts/keygen_master.sh
@@ -0,0 +1,12 @@
+echo "Starting Script"
+cd ~
+mkdir .ssh
+echo "\n\n\n" | ssh-keygen -t rsa -P ""	
+cat /home/pi/.ssh/id_rsa.pub >> /home/pi/.ssh/authorized_keys	
+cd ~/.ssh
+chmod 600 authorized_keys
+Cd ~/.ssh
+scp id_rsa.pub pi@raspberrypislave1:/home/pi/.ssh/authorized_keys
+scp id_rsa.pub pi@raspberrypislave2:/home/pi/.ssh/authorized_keys
+scp id_rsa.pub pi@raspberrypislave3:/home/pi/.ssh/authorized_keys
+scp id_rsa.pub pi@raspberrypislave4:/home/pi/.ssh/authorized_keys
