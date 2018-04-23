5056d17e3f53e44ea32a26fab169368d7c172eb2
karankotz <karankotz@gmail.com>
2018-04-22 21:01:13 -0400

    Updated Makefile

---
+++ project-code/Hadoop-Spark-Yarn/Hadoop Java WordCount Applicaton/run-app.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+cd /opt/hadoop-2.7.1/sbin/
+
+sh start-all.sh
+
+# cd $HADOOP_CONF
+
+# echo “export HADOOP_CLASSPATH=/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt/lib/tools.jar” >> /hadoop-env.sh
+
+cd /home/hduser 
+
+mkdir wordcount
+
+mkdir input 
+
+sudo wget -P /home/hduser/wordcount/input/ https://www.dropbox.com/s/oykmrl6okxqthsn/Bhagvadgita.txt
+                                                            
+
+hadoop dfs -copyFromLocal /home/hduser/wordcount/input/Bhagvadgita.txt /home/hduser/wordcount/input/
+
+cd /scripts/
+
+sudo /opt/hadoop-2.7.1/bin/hadoop com.sun.tools.javac.Main WordCount.java
+
+sudo jar cf wc.jar WordCount*.class 
+
+hadoop jar wc.jar WordCount /home/hduser/wordcount/input/ /home/hduser/wordcount/output/output-java
+
+
+
