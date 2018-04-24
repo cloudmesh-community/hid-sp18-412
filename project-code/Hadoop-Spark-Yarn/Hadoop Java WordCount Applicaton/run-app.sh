7e369ba8d8650e74c5ec36b31ba3315f5693c61d
karankotz <karankotz@gmail.com>
2018-04-24 02:34:47 -0400

    Updated Automated Script for Hadoop Java Word Count

--- project-code/Hadoop-Spark-Yarn/Hadoop Java WordCount Applicaton/run-app.sh
+++ project-code/Hadoop-Spark-Yarn/Hadoop Java WordCount Applicaton/run-app.sh
@@ -4,9 +4,9 @@ cd /opt/hadoop-2.7.1/sbin/
 
 sh start-all.sh
 
-# cd $HADOOP_CONF
+cd $HADOOP_CONF
 
-# echo “export HADOOP_CLASSPATH=/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt/lib/tools.jar” >> /hadoop-env.sh
+echo “export HADOOP_CLASSPATH=/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt/lib/tools.jar” >> /hadoop-env.sh
 
 cd /home/hduser 
 
@@ -14,12 +14,17 @@ mkdir wordcount
 
 mkdir input 
 
+cd /home/hduser/wordcount/input 
+
 sudo wget -P /home/hduser/wordcount/input/ https://www.dropbox.com/s/oykmrl6okxqthsn/Bhagvadgita.txt
-                                                            
+
+hadoop fs -mkdir -p /home/hduser/wordcount/input/                                                                                        t
 
 hadoop dfs -copyFromLocal /home/hduser/wordcount/input/Bhagvadgita.txt /home/hduser/wordcount/input/
 
 cd /scripts/
+
+#hadoop com.sun.tools.javac.Main WordCount.java
 
 sudo /opt/hadoop-2.7.1/bin/hadoop com.sun.tools.javac.Main WordCount.java
 
